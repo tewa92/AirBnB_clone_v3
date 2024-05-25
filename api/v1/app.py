633075a167ba3e2f436b31325040301d57ff1edc
tewa92 <tewachewsaleigizer@gmail.com>
2024-05-25 01:52:10 -0700

    Airbnb clone RESTful API

--- api/v1/app.py
+++ api/v1/app.py
@@ -7,9 +7,11 @@ import os
 app = Flask(__name__)
 app.register_blueprint(app_views)
 
+
 @app.teardown_appcontext
 def teardown_db(exception):
     storage.close()
+
 
 if __name__ == "__main__":
     host = os.getenv('HBNB_API_HOST', '0.0.0.0')
--- api/v1/views/app_views.py
+++ api/v1/views/app_views.py
@@ -2,6 +2,7 @@ from flask import Blueprint, jsonify
 
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
 
+
 @app_views.route('/status', methods=['GET'])
 def status():
     return jsonify({"status": "OK"})
