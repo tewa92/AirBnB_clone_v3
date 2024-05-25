0070152220d9d4d564a9ec5a0f6f11997674f2ab
tewa92 <tewachewsaleigizer@gmail.com>
2024-05-24 22:16:46 -0700

    Airbnb clone

--- tests/test_models/test_engine/test_db_storage.py
+++ tests/test_models/test_engine/test_db_storage.py
@@ -90,17 +90,17 @@ class TestFileStorage(unittest.TestCase):
 
 class TestDatabaseStorage(unittest.TestCase):
     def setUp(self):
-        self.storage = DatabaseStorage()
-        self.my_class_instance = MyClass(1, "test")
+        self.storage = db_storage()
+        self.my_class_instance = State(1, "test")
 
     def test_get(self):
         """Test the get method."""
         self.storage.save(self.my_class_instance)
-        retrieved_object = self.storage.get(MyClass, 1)
+        retrieved_object = self.storage.get(State, 1)
         self.assertEqual(retrieved_object, self.my_class_instance)
 
     def test_count_all(self):
         """Test the count method."""
         self.storage.save(self.my_class_instance)
-        count = self.storage.count(MyClass)
+        count = self.storage.count(State)
         self.assertEqual(count, 1)
