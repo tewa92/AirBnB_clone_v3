0241b7a041c556631e9325ae104c32e75a3f2fbe
tewa92 <tewachewsaleigizer@gmail.com>
2024-05-25 02:43:25 -0700

    Airbnb RESTfulapi

--- api/v1/app.py
+++ api/v1/app.py
@@ -16,4 +16,4 @@ def teardown_db(exception):
 if __name__ == "__main__":
     host = os.getenv('HBNB_API_HOST', '0.0.0.0')
     port = os.getenv('HBNB_API_PORT', 5000)
-    app.run(host=host, port=int(port), threaded=True)
+    app.run(host=host, port=int(port), threaded=True, debug=True)
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,6 +1,6 @@
 # api/v1/views/index.py
 from flask import jsonify
-from api.v1.views import app_views
+from . import app_views
 
 
 @app_views.route('/status', methods=['GET'])
