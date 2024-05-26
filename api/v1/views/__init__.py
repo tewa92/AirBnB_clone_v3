909c66e8b64263521f13b1f5e3eceb9c41a13739
tewa92 <tewachewsaleigizer@gmail.com>
2024-05-26 04:42:57 -0700

    Airbnv restful api

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -4,6 +4,7 @@ This module sets up the Blueprint for the API views.
 """
 
 from flask import Blueprint
-from api.v1.views.index import *
 
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
+
+from api.v1.views.index import *
