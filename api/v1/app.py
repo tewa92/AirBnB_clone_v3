52b96e951a60b1a727459aaffaa735c34c992bf8
tewa92 <tewachewsaleigizer@gmail.com>
2024-05-26 04:40:37 -0700

    Airbnv restful api

--- api/v1/app.py
+++ api/v1/app.py
@@ -11,12 +11,14 @@ import os
 app = Flask(__name__)
 app.register_blueprint(app_views)
 
+
 @app.teardown_appcontext
 def teardown(exception):
     """
     Close the storage on teardown.
     """
     storage.close()
+
 
 if __name__ == "__main__":
     host = os.getenv('HBNB_API_HOST', '0.0.0.0')
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -6,6 +6,7 @@ This module defines the index view for the API.
 from api.v1.views import app_views
 from flask import jsonify
 
+
 @app_views.route('/status', methods=['GET'])
 def status():
     """
