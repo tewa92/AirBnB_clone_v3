e249c2664a9ecbde03a7c3364288e6d3741b97eb
tewa92 <tewachewsaleigizer@gmail.com>
2024-05-25 23:01:43 -0700

    Flask app

--- api/v1/app.py
+++ api/v1/app.py
@@ -13,6 +13,12 @@ def teardown_db(exception):
     storage.close()
 
 
+@app.errorhandler(404)
+def not_found(error):
+    """Handle 404 errors and return a JSON response."""
+    return jsonify({"error": "Not found"}), 404
+
+
 if __name__ == "__main__":
     host = os.getenv('HBNB_API_HOST', '0.0.0.0')
     port = os.getenv('HBNB_API_PORT', 5000)
