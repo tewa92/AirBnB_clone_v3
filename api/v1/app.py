b8332592a678d3a8c8f7b906e6402d50c986f4b4
tewa92 <tewachewsaleigizer@gmail.com>
2024-05-26 04:51:34 -0700

    Airbnv restful api

--- api/v1/app.py
+++ api/v1/app.py
@@ -23,4 +23,4 @@ def teardown(exception):
 if __name__ == "__main__":
     host = os.getenv('HBNB_API_HOST', '0.0.0.0')
     port = int(os.getenv('HBNB_API_PORT', 5000))
-    app.run(host=host, port=port, threaded=True, debug=True)
+    app.run(host=host, port=port, threaded=True)
