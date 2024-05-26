c7fd676fd60ff4151a749ee084b8f8cf47b9ff16
tewa92 <tewachewsaleigizer@gmail.com>
2024-05-25 22:53:02 -0700

    Airbnb

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,8 +1,20 @@
 # api/v1/views/index.py
 from flask import jsonify
-from . import app_views
-
+from api.v1.views import app_views
+from models import storage
 
 @app_views.route('/status', methods=['GET'])
 def get_status():
     return jsonify({"status": "OK"})
+
+@app_views.route('/stats', methods=['GET'])
+def get_stats():
+    stats = {
+        "amenities": storage.count("amenities"),
+        "cities": storage.count("cities"),
+        "places": storage.count("places"),
+        "reviews": storage.count("reviews"),
+        "states": storage.count("states"),
+        "users": storage.count("users")
+    }
+    return jsonify(stats)
